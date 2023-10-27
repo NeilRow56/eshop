a642afdaa18c493d7b6e8b082d712c88a08bc310
NeilRow56 <neilrowland56@gmail.com>
2023-10-27 09:55:56 +0100

    Amended home page to list dummy products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Container from '@/components/Container'
 import HomeBanner from '@/components/nav/HomeBanner'
+import { products } from '@/utils/products'
+import { truncateText } from '@/utils/truncateText'
 
 export default function Home() {
   return (
@@ -7,6 +9,12 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner />
+        </div>
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {products.map((product: any) => {
+            // eslint-disable-next-line react/jsx-key
+            return <div>{truncateText(product.name)}</div>
+          })}
         </div>
       </Container>
     </div>
---
+++ src/utils/truncateText.ts
@@ -0,0 +1,12 @@
+// export const truncateText = (str: string) => {
+//   if (str.length < 25) return str
+
+//   return str.substring(0, 25) + '...'
+// }
+
+export const truncateText = (str: string, max = 5) => {
+  const array = str.trim().split(' ')
+  const ellipsis = array.length > max ? '...' : ''
+
+  return array.slice(0, max).join(' ') + ellipsis
+}
