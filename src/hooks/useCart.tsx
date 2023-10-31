f337de7ce8a183c940a59a83b8edfbbd37adc7fc
NeilRow56 <neilrowland56@gmail.com>
2023-10-31 08:08:31 +0000

    Added loval storage to retain cart items upon refresh

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,13 @@
 //cart context
 
 import { CartProductType } from '@/components/products/ProductDetails'
-import { createContext, useCallback, useContext, useState } from 'react'
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 
 type CartContextType = {
   cartTotalQty: number
@@ -21,6 +27,13 @@ export const CartContextProvider = (props: Props) => {
     null
   )
 
+  useEffect(() => {
+    const cartItems: any = localStorage.getItem('eSopCartItems')
+    const cProducts: CartProductType[] | null = JSON.parse(cartItems)
+
+    setCartProducts(cProducts)
+  }, [])
+
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
       let updatedCart
@@ -30,6 +43,7 @@ export const CartContextProvider = (props: Props) => {
       } else {
         updatedCart = [product]
       }
+      localStorage.setItem('eSopCartItems', JSON.stringify(updatedCart))
       return updatedCart
     })
   }, [])
